8274505031930d13d8c851121617f1421f063d05
tigreeroo <ganz67@mail.ru>
2025-09-15 19:20:59 +0300

    create init file

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,6 +1,11 @@
 from .db_helper import db_helper
+from .base import Base
+from .review import Review
+from .response import Response
 
 __all__ = (
     'db_helper',
-
+    'Base',
+    'Review',
+    'Response'
 )
