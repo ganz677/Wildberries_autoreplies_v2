ff4965eab69b464c3417b885165d2388ff7a5895
tigreeroo <ganz67@mail.ru>
2025-09-16 14:36:52 +0300

    add classes to __all__

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,11 @@
 from .fetcher import FetchNewReviewsService
 from .replier import GenerateRepliesService
+from .publisher import PublishRepliesService
+from .pipeline import ReviewsPipline
 
 __all__ = (
     'FetchNewReviewsService',
     'GenerateRepliesService',
+    'PublishRepliesService',
+    'ReviewsPipline'
 )
